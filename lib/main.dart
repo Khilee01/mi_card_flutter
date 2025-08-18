8382666ce27d25cdd2af74010258d4b7b68257be
Khilee01 <khileesinghal@gmail.com>
2025-08-19 01:15:12 +0530

    Small project for displaying details usimg ListTile Card Icon CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(width: 110.0, color: Colors.red),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(height: 110.0, width: 110.0, color: Colors.yellow),
-                  Container(height: 110.0, width: 110.0, color: Colors.green),
-                ],
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
-              Container(width: 110.0, color: Colors.blue),
+              Text(
+                "Khilee Singhal",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.7,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+                width: 200,
+                child: Divider(color: Colors.white),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal.shade900),
+                    trailing: Text(
+                      "+91 7073793038",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal.shade900),
+                    trailing: Text(
+                      "khileesinghal@gmail.com",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: Colors.teal.shade800,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
